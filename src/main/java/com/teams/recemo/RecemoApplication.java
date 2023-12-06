18968850daa9b2be39db6a46a2ce525ee3201fd2
jo <yuyule20@naver.com>
2023-12-06 09:47:54 +0900

    first_test

---
+++ src/main/java/com/teams/recemo/RecemoApplication.java
@@ -0,0 +1,13 @@
+package com.teams.recemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teams/recemo/RecemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teams.recemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
